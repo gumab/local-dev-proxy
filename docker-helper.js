779cfe1a7df197cbdc5df5e6fd434e6fa6ca3959
배창원 <changwon.bae@kurlycorp.com>
2023-08-28 01:56:33 +0900

    도커 실행 관련 오류처리

--- docker-helper.js
+++ docker-helper.js
@@ -19,17 +19,20 @@ const execAsync = (command) => new Promise(
     });
 
 async function run() {
-  try {
-    const ps = await execAsync('docker ps|grep 0.0.0.0:80').
-        then(x => /^\w+/.exec(x)?.[0]);
-    if (ps) {
-      await execAsync(`docker stop ${ps}`);
-    }
-  } catch (e) {
+  const ps = await execAsync('docker ps|grep 0.0.0.0:80').
+      then(x => /^\w+/.exec(x)?.[0]).catch(() => undefined);
+  if (ps) {
+    await execAsync(`docker stop ${ps}`);
   }
-  await execAsync('docker rm -f local-dev-proxy');
+  await execAsync('docker rm -f local-dev-proxy').catch(() => {
+    throw new Error('[local-dev-proxy] 도커 설치 후 이용해주세요.');
+  });
   await execAsync(
-      'docker run -d --name local-dev-proxy -p 80:8080 -p 443:8443 ghcr.io/thefarmersfront/local-dev-proxy:latest');
+      'docker run -d --name local-dev-proxy -p 80:8080 -p 443:8443 ghcr.io/thefarmersfront/local-dev-proxy:latest').
+      catch(() => {
+        throw new Error(
+            '[local-dev-proxy] 도커 실행에 실패하였습니다. 80/443 포트 점유 확인 후 다시 이용해주세요.');
+      });
 }
 
 module.exports.run = run;
