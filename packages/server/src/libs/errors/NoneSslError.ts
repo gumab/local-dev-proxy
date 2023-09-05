affb20ed5d210161b4b21e42ef68c2b29769194b
배창원 <changwon.bae@kurlycorp.com>
2023-09-05 09:37:53 +0900

    https->http redirect

---
+++ packages/server/src/libs/errors/NoneSslError.ts
@@ -0,0 +1,1 @@
+export class NoneSslError extends Error {}
--- packages/server/src/routes/proxy.ts
+++ packages/server/src/routes/proxy.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import { createProxyServer } from 'http-proxy';
 import { storage } from '../storage';
 import { NotFoundError } from '../libs/errors/NotFoundError';
+import { NoneSslError } from '../libs/errors/NoneSslError';
 
 const proxyServer = createProxyServer({
   secure: false,
@@ -46,7 +47,7 @@ function makeProxyOption(req: Request): { target: string; ignorePath?: boolean; 
   if (!rule) {
     throw new NotFoundError(`매칭되는 서버가 없습니다. (${req.protocol}://${req.hostname}${req.path})`);
   } else if (!rule.https && req.protocol === 'https') {
-    throw new NotFoundError(
+    throw new NoneSslError(
       `HTTPS 가 설정되지 않았습니다. .ldprxrc.js 파일을 확인해주세요 (${req.protocol}://${req.hostname}${req.path})`,
     );
   }
@@ -74,6 +75,8 @@ export default function (req: Request, res: Response) {
   } catch (e) {
     if (e instanceof NotFoundError) {
       res.status(404).send(`[local-dev-proxy] ${e.message}`);
+    } else if (e instanceof NoneSslError) {
+      res.redirect(`http://${req.hostname}${req.url}`);
     } else if (e instanceof Error) {
       res.status(500).send(`[local-dev-proxy] ${e.message}`);
     } else {
